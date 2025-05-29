3417db36b205bb1c2bffaf1258e91fb46b9f7a42
SamuelSous0 <samuel.00000859887@unicap.br>
2025-05-29 20:00:19 -0300

    Update pptpve.js

--- Projeto_Web_Central_De_Jogos-main/pedrapapeltesousapve/pptpve.js
+++ Projeto_Web_Central_De_Jogos-main/pedrapapeltesousapve/pptpve.js
@@ -38,3 +38,11 @@ function jogar() {
     res.innerHTML = `<p>${resultadoTexto}</p>`;
     res.className = classeResultado;
 }
+
+function Mudarestado(el) {
+        var display = document.getElementById(el).style.display;
+        if(display == "none")
+            document.getElementById(el).style.display = 'block';
+        else
+            document.getElementById(el).style.display = 'none';
+    }
